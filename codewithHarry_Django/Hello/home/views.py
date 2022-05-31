7f320bc2867bbba8ad71c89ebf543d64c378aa86
Ctushar721 <tushar.choudhary010@gmail.com>
2022-06-01 00:52:55 +0530

    Done Basic Part of the video

--- codewithHarry_Django/Hello/home/views.py
+++ codewithHarry_Django/Hello/home/views.py
@@ -1,11 +1,18 @@
 # from django.shortcuts import render_to_response
 from urllib3 import HTTPResponse
 from django.shortcuts import HttpResponse, render
+from home.models import Contact
 
 def index(request):
     context = {
         'x':'Variable'
     }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        contact = Contact( name= name,email=email)
+        contact.save()
+        print(name,email)
     return render(request, 'index.html', context)
     # return HttpResponse('This is homepage')
 def about(request):
